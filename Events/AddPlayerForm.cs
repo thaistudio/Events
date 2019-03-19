d9baef8ead94d93130edc723cae024f92995d29d
Thai Son Nguyen <TS.Nguyen@StrucSoftSolutions.com>
2019-03-19 19:55:09 -0400

    Events practice

---
+++ Events/AddPlayerForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Events
+{
+   
+    public partial class AddPlayerForm : Form
+    {
+        Squad squad = new Squad();
+        List<Player> updatePlayers = new List<Player>();
+
+
+        public AddPlayerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Form1 form1 = new Form1();
+            form1.playersForm1.Add(new Player() { Name = nameBox.Text, Number = Convert.ToInt32(numberBox.Text), Attribute = Convert.ToInt32(attributeBox.Text) });
+            form1.UpdateSquad();
+            //foreach (Player player in form1.playersForm1)
+            //{
+            //    form1.name.Add(nameBox.Text);
+            //    form1.numbers.Add(Convert.ToInt32(numberBox.Text));
+            //    form1.attributes.Add(Convert.ToInt32(attributeBox.Text));
+            //}
+        }
+    }
+}
--- Events/Program.cs
+++ Events/Program.cs
@@ -18,5 +18,43 @@ namespace Events
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int Attribute { get; set; }
+       
+    }
+
+    public class Squad
+    {
+        public List<Player> players;
+
+        public Squad()
+        {
+            players = new List<Player>();
+        }
+
+        public List<Player> CreateSquad()
+        {
+            players.Add(new Player() { Name = "Thai", Number = 20, Attribute = 90 });
+            players.Add(new Player() { Name = "Lingard", Number = 14, Attribute = 82 });
+            players.Add(new Player() { Name = "Pogba", Number = 6, Attribute = 86 });
+            players.Add(new Player() { Name = "Dalot", Number = 22, Attribute = 72 });
+
+            return players;
+        }
+
+        public bool CheckSquad()
+        {
+            if (players.Count > 6)
+                return false;
+            else
+                return true;
+        }
     }
 }
